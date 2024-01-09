15761da3bd1decce57a0aa9a97bcd86b54ef293a
A1_frkweiichi <engineer.frkw@gmail.com>
2024-01-09 13:51:27 +0900

    second commit

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total_drinks = amount
+    empty_bottles = amount
+
+    while empty_bottles >= 3
+      new_drinks = empty_bottles / 3
+      total_drinks += new_drinks
+      empty_bottles = empty_bottles % 3 + new_drinks
+    end
+
+    total_drinks
   end
 end
